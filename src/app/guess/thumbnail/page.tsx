c5447ec57e4ebdde89c3bfae0ea691cffad51d0d
ToastedToast <hey@toasted.dev>
2024-02-12 14:16:37 +0800

    feat(seo): add titles for other pages

--- src/app/guess/thumbnail/page.tsx
+++ src/app/guess/thumbnail/page.tsx
@@ -2,8 +2,14 @@ import { db } from "@/db";
 import Guess from "../guess";
 import { notFound } from "next/navigation";
 import { randomChoice } from "@/lib/utils";
+import { Metadata } from "next";
 
-export default async function GuessByTitle() {
+export const metadata: Metadata = {
+  title: "Guess By Thumbnail",
+  description: "Guess your favorite statistics creators by their thumbnails.",
+};
+
+export default async function GuessByThumbnail() {
   const videos = await db.query.videos.findMany({
     with: {
       channel: true,
--- src/app/guess/title/page.tsx
+++ src/app/guess/title/page.tsx
@@ -2,6 +2,12 @@ import { db } from "@/db";
 import Guess from "../guess";
 import { notFound } from "next/navigation";
 import { randomChoice } from "@/lib/utils";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Guess By Title",
+  description: "Guess your favorite statistics creators by their titles.",
+};
 
 export default async function GuessByTitle() {
   const videos = await db.query.videos.findMany({
